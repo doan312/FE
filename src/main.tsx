da5ab87ff8061ee951c9424092a8c8033a2d42e7
dohan <100039401+doan312@users.noreply.github.com>
2025-02-11 23:44:18 +0900

    프로젝트 기본 세팅 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom"; // React Router import 추가
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/8hz-FE">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
