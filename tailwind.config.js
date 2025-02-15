cebd92611c32df5084a006375b8b0941b62a5f9d
Jisun Park <jisunpark.t@gmail.com>
2025-02-15 16:33:04 +0900

    타이포그래피 및 컬러 시스템 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,91 +1,129 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'green-custom': '#AAB396',
-        'brwon-custom': '#674636',
-        'yellow-custom':  '#F7EED3',
-        'white-custom': '#F9F9F9',
-      },
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
-      },
-      backgroundImage: {
-        'gradient-to-t': 'linear-gradient(to top, rgba(75, 75, 75, 0) 0%, rgba(120, 120, 120, 1) 100%)',
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'purple-30': '#EEE5FF',
+                'purple-50': '#D8C4FC',
+                'purple-100': '#CDB3FB',
+                'purple-200': '#B691FA',
+                'purple-300': '#A070F8',
+                'purple-400': '#894EF7',
+                'purple-500': '#732CF5',
+                'purple-600': '#6125CE',
+                'purple-700': '#4E1EA7',
+                'purple-800': '#3C177F',
+                'purple-900': '#291058',
+                'gray-100': '#FFFFFF',
+                'gray-200': '#FAFAFA',
+                'gray-300': '#F5F5F5',
+                'gray-400': '#F0F0F0',
+                'gray-500': '#D9D9D9',
+                'gray-600': '#BFBFBF',
+                'gray-700': '#8C8C8C',
+                'gray-800': '#595959',
+                'gray-900': '#434343',
+                'gray-1000': '#262626',
+                'gray-1100': '#1F1F1F',
+                'gray-1200': '#141414',
+                'gray-1300': '#111111',
+                'BN-pink': '#FFA3A5',
+                'BN-blue': '#C7E3FF',
+                'BN-green': '#C6D96F',
+                'BN-yellow': '#FFDC6B',
+                'SR-yellow': '#FFB731',
+                'BG-blue': '#ECEFFF',
+                'TXT-blue': '#6173FD',
+                'BG-pink': '#FFECEC',
+                'TXT-pink': '#FA95A2',
+            },
+            fontSize: {
+                h1: ['32px', { lineHeight: '140%', letterSpacing: '-0.5%' }],
+                h2: ['24px', { lineHeight: '140%', letterSpacing: '-0.5%' }],
+                h3: ['20px', { lineHeight: '140%', letterSpacing: '-0.5%' }],
+                h4: ['18px', { lineHeight: '140%', letterSpacing: '-0.5%' }],
+                body1: ['16px', { lineHeight: '140%', letterSpacing: '-0.5%' }],
+                body2: ['14px', { lineHeight: '140%', letterSpacing: '-0.5%' }],
+                caption: [
+                    '12px',
+                    { lineHeight: '140%', letterSpacing: '-0.5%' },
+                ],
+            },
+            fontFamily: {
+                sans: ['Roboto', 'sans-serif'],
+            },
+            backgroundImage: {
+                'gradient-to-t':
+                    'linear-gradient(to top, rgba(75, 75, 75, 0) 0%, rgba(120, 120, 120, 1) 100%)',
+            },
 
+            animation: {
+                'bounce-once': 'bounce 1s infinite 5s', // 5초마다 바운스 애니메이션 실행
+            },
+            keyframes: {
+                bounce: {
+                    '0%, 100%': {
+                        transform: 'translateY(-25%)',
+                        animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+                    },
+                    '50%': {
+                        transform: 'none',
+                        animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+                    },
+                },
+            },
 
-
-
-      },
-
-      animation: {
-        'bounce-once': 'bounce 1s infinite 5s' // 5초마다 바운스 애니메이션 실행
-      },
-      keyframes: {
-        bounce: {
-          '0%, 100%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0.8,0,1,1)' },
-          '50%': { transform: 'none', animationTimingFunction: 'cubic-bezier(0,0,0.2,1)' },
+            rotate: {
+                180: '-180deg', // 180도 회전
+            },
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '1' },
+                },
+            },
+            animation: {
+                fadeIn: 'fadeIn 1s ease-out',
+            },
+            height: {
+                50: '200px', // lg 화면
+                75: '250px', // md 화면
+                100: '275px', // 기본값
+            },
+            spacing: {
+                '15vh': '17vh',
+                '20vh': '20vh',
+            },
+            width: {
+                'screen-60': '60vw', // 화면 너비의 60%
+            },
+            animation: {
+                'bounce-200': 'bounce 1s infinite 200ms', // 200ms 지연
+                'bounce-400': 'bounce 1s infinite 400ms', // 400ms 지연
+                'bounce-600': 'bounce 1s infinite 600ms', // 600ms 지연
+                'bounce-800': 'bounce 1s infinite 800ms', // 800ms 지연
+            },
+            animation: {
+                'slide-out': 'slideOut 1s forwards',
+                'slide-in': 'slideIn 1s forwards',
+            },
+            // 키프레임 정의 추가
+            keyframes: {
+                slideOut: {
+                    from: { opacity: 1, transform: 'translateX(0%)' },
+                    to: { opacity: 0, transform: 'translateX(-100%)' },
+                },
+                slideIn: {
+                    from: { opacity: 0, transform: 'translateX(100%)' },
+                    to: { opacity: 1, transform: 'translateX(0%)' },
+                },
+            },
+            screens: {
+                custom: '923px', // Adds a custom breakpoint at 1023px
+            },
         },
-      },
-
-      
-      rotate: {
-        '180': '-180deg', // 180도 회전
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-      },
-      animation: {
-        fadeIn: 'fadeIn 1s ease-out',
-      },
-      height: {
-        '50': '200px',  // lg 화면
-        '75': '250px',  // md 화면
-        '100': '275px', // 기본값
-      },
-      spacing: {
-        '15vh': '17vh',
-        '20vh': '20vh',
-      },
-      width: {
-        'screen-60': '60vw', // 화면 너비의 60%
-      },
-      animation: {
-        'bounce-200': 'bounce 1s infinite 200ms', // 200ms 지연
-        'bounce-400': 'bounce 1s infinite 400ms', // 400ms 지연
-        'bounce-600': 'bounce 1s infinite 600ms', // 600ms 지연
-        'bounce-800': 'bounce 1s infinite 800ms', // 800ms 지연
-      },
-      animation: {
-        'slide-out': 'slideOut 1s forwards',
-        'slide-in': 'slideIn 1s forwards',
-      },
-      // 키프레임 정의 추가
-      keyframes: {
-        slideOut: {
-          from: { opacity: 1, transform: 'translateX(0%)' },
-          to: { opacity: 0, transform: 'translateX(-100%)' },
-        },
-        slideIn: {
-          from: { opacity: 0, transform: 'translateX(100%)' },
-          to: { opacity: 1, transform: 'translateX(0%)' },
-        },
-      },
-      screens: {
-        'custom': '923px'  // Adds a custom breakpoint at 1023px
-      }
     },
-  },
-  
-  plugins: [
-    
-  ],
+
+    plugins: [],
 }
