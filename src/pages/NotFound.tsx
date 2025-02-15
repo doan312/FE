73ab242a6dcd792bae6f690c4f8ee53187e95cbe
dohan <100039401+doan312@users.noreply.github.com>
2025-02-15 14:40:43 +0900

    404 NotFound 페이지 추가

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,18 @@
+//404 페이지 찾을수 없음
+
+import React from "react";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-screen bg-gray-100 p-4">
+      <div className="flex flex-col md:flex-row w-full max-w-7xl max-h-screen bg-white border border-gray-300 rounded-lg shadow-lg overflow-hidden">
+        <div className="md:w-1/2 p-6 md:p-8 lg:p-10 text-left flex flex-col justify-center">
+          <p className="text-5xl font-bold text-blue-600">test</p>
+          <p className="text-lg text-gray-600">test</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
