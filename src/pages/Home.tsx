8aa873ce911d90e2f26a4ab5047b26db123c8600
Jisun Park <jisunpark.t@gmail.com>
2025-02-15 23:16:02 +0900

    design: 상담 방식 선택 영역

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,19 @@
 
 import React from 'react'
 import Logo from '../components/home/Logo'
+import MethodSelectionCard from '../components/home/MethodSelectionCard'
 
 const Home: React.FC = () => {
     return (
-        <div className='h-[852px] w-full bg-gradient-to-b from-purple-50 to-gray-100'>
+        <div className='h-[852px] w-full bg-gradient-to-b from-purple-50 to-gray-100 px-16'>
             <Logo />
+            <h2 className='mb-16 font-bold text-h2 text-gray-1300'>
+                어떤 방식으로 상담 받을까요?
+            </h2>
+            <div className='flex gap-11'>
+                <MethodSelectionCard type='inPerson' />
+                <MethodSelectionCard type='online' />
+            </div>
         </div>
     )
 }
