ec007233cc7fa8cdde3e56e3048b2cc65cf7bd11
Jisun Park <jisunpark.t@gmail.com>
2025-02-16 14:29:23 +0900

    desig: 배너 컴포넌트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@
 import React from 'react'
 import Logo from '../components/home/Logo'
 import MainSection from '../components/home/MainSection'
+import Banner from '../components/home/Banner'
 
 const Home: React.FC = () => {
     return (
         <div className='h-[852px] w-full bg-gradient-to-b from-purple-50 to-gray-100 px-16'>
             <Logo />
             <MainSection />
+            <Banner />
         </div>
     )
 }
