e5f1473c6a0bf9087c22b22879c5cebcce3f7864
Jisun Park <jisunpark.t@gmail.com>
2025-02-16 21:20:51 +0900

    design: 배너 스와이퍼

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -8,7 +8,7 @@ export default function TabBar() {
     return (
         <div className='pb-26 fixed bottom-0 left-0 right-0 z-50 flex w-full items-center justify-between bg-gray-100 px-80 pt-10 shadow-[0_-2px_13px_rgba(0,0,0,0.08)]'>
             {tabList.map((tab) => (
-                <TabItem type={tab} />
+                <TabItem key={tab} type={tab} />
             ))}
         </div>
     )
---
+++ src/components/home/BannerSwiper.tsx
@@ -0,0 +1,32 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import Banner from './Banner'
+import { Autoplay } from 'swiper/modules'
+
+export default function BannerSwiper() {
+    return (
+        <Swiper
+            spaceBetween={12}
+            slidesPerView={1}
+            centeredSlides={true}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            grabCursor={true}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+            }}
+            modules={[Autoplay]}
+            className='mb-44'>
+            <SwiperSlide>
+                <Banner />
+            </SwiperSlide>
+            <SwiperSlide>
+                <Banner />
+            </SwiperSlide>
+            <SwiperSlide>
+                <Banner />
+            </SwiperSlide>
+        </Swiper>
+    )
+}
