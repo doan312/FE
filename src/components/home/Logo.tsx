ff9588f3664629d5b9bc922642cb34ecf6511bcc
Jisun Park <jisunpark.t@gmail.com>
2025-02-16 21:20:51 +0900

    design: 배너 스와이퍼

--- src/components/home/Logo.tsx
+++ src/components/home/Logo.tsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
 import LogoIcon from '../../../public/img/logo.svg'
 
 export default function Logo() {
     return (
-        <div className='flex flex-col justify-center w-full h-104 pt-47'>
+        <Link
+            to='/'
+            className='flex flex-col justify-center w-full cursor-pointer h-104 pt-47'>
             <img src={LogoIcon} alt='로고 아이콘' className='self-start h-24' />
-        </div>
+        </Link>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,19 @@
 import React from 'react'
 import Logo from '../components/home/Logo'
 import MainSection from '../components/home/MainSection'
-import Banner from '../components/home/Banner'
 import RescheduleBanner from '../components/home/RescheduleBanner'
 import HotNewSection from '../components/home/HotNewSection'
 import BeforeAfterSection from '../components/home/BeforeAfterSection'
 import TabBar from '../components/TabBar/TabBar'
+import BannerSwiper from '../components/home/BannerSwiper'
 
 const Home: React.FC = () => {
     return (
-        <div className='relative w-full px-16 bg-gray-100 -z-10 pb-155'>
-            <div className='absolute left-0 right-0 top-0 -z-10 -m-16 h-[852px] bg-gradient-to-b from-purple-50 to-gray-100'></div>
+        <div className='relative z-0 w-full px-16 bg-gray-100 pb-155'>
+            <div className='absolute left-0 right-0 top-0 -z-20 -m-16 h-[852px] bg-gradient-to-b from-purple-50 to-gray-100'></div>
             <Logo />
             <MainSection />
-            <Banner />
+            <BannerSwiper />
             <RescheduleBanner />
             <HotNewSection />
             <BeforeAfterSection />
