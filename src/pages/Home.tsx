c2691f362494f5d49c8a4ef2571ada0be277aedc
Jisun Park <jisunpark.t@gmail.com>
2025-02-16 20:12:30 +0900

    design: 배경색 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ import TabBar from '../components/TabBar/TabBar'
 
 const Home: React.FC = () => {
     return (
-        <div className='relative w-full px-16 pb-155'>
+        <div className='relative w-full px-16 bg-gray-100 -z-10 pb-155'>
             <div className='absolute left-0 right-0 top-0 -z-10 -m-16 h-[852px] bg-gradient-to-b from-purple-50 to-gray-100'></div>
             <Logo />
             <MainSection />
