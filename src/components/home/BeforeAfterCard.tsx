9d3499a48f271b2825a0a12b53cb0215186c2ef0
Jisun Park <jisunpark.t@gmail.com>
2025-02-17 08:37:18 +0900

    feat: 비포애프터 프롭 전달

--- src/components/home/BeforeAfterCard.tsx
+++ src/components/home/BeforeAfterCard.tsx
@@ -1,24 +1,38 @@
 import { Link } from 'react-router-dom'
-import Image from '../../../public/img/before-after-0.png'
 import Rating from './Rating'
 
-export default function BeforeAfterCard() {
+interface Props {
+    title: string
+    designer: string
+    rating: number
+    review: string
+    bgImg: string
+}
+
+export default function BeforeAfterCard({
+    title,
+    designer,
+    rating,
+    review,
+    bgImg,
+}: Props) {
     return (
         <Link
             to='#'
             className='relative h-[320px] w-[256px] shrink-0 overflow-hidden rounded-10'>
-            <img src={Image} alt='비포애프터 이미지' />
-            <div className='absolute bottom-0 left-0 right-0 w-full px-14 pb-14'>
-                <span className='font-bold text-gray-100 text-h4'>
-                    단발에서 긴머리로 변신 💇🏻‍♀️
-                </span>
-                <div className='flex items-center gap-4 text-gray-100 text-body2'>
-                    <span>김서연 디자이너</span>
-                    <Rating rating={4.7} textColor='white' />
+            <img src={bgImg} alt='비포애프터 이미지' />
+            <div className='absolute bottom-0 left-0 right-0 flex flex-col w-full gap-13 px-14 pb-14'>
+                <div className='flex flex-col'>
+                    <span className='font-bold text-gray-100 text-h4'>
+                        {title}
+                    </span>
+                    <div className='flex items-center gap-4 text-gray-100 text-body2'>
+                        <span>{designer}</span>
+                        <Rating rating={rating} textColor='white' />
+                    </div>
                 </div>
-                <span className='text-caption text-[#e5e5e5]'>
-                    이번이 두번재 예약인데 기억해주시더라구요!! 일단 감동하면서
-                    앉았습니다. 처음 자를 때 너무 깔...
+                <span className='line-clamp-2 text-caption text-[#e5e5e5]'>
+                    {review}
                 </span>
             </div>
         </Link>
--- src/components/home/BeforeAfterSection.tsx
+++ src/components/home/BeforeAfterSection.tsx
@@ -1,4 +1,29 @@
 import BeforeAfterCard from './BeforeAfterCard'
+import BgImage from '../../../public/img/before-after-0.png'
+
+const beforeAfterData = [
+    {
+        title: '단발에서 긴머리로 변신 💇🏻‍♀️',
+        designer: '김서연 디자이너',
+        rating: 4.7,
+        review: '이번이 두번재 예약인데 기억해주시더라구요!! 일단 감동하면서 앉았습니다. 처음 자를 때 너무 깔꼼하게 잘라줘가꾸 또 예약했는데 더 마음에 들어용☺️ 주변 친구들이 다니던 미용실 중 옆 뒤가 젤 깔끔하다고 하고, 스몰톡도 야무져서 지루하지도 않았어요. 상담 고민중이시면 꼭 추천드립니다.',
+        bgImg: BgImage,
+    },
+    {
+        title: '단발에서 긴머리로 변신 💇🏻‍♀️',
+        designer: '김서연 디자이너',
+        rating: 4.7,
+        review: '이번이 두번재 예약인데 기억해주시더라구요!! 일단 감동하면서 앉았습니다. 처음 자를 때 너무 깔꼼하게 잘라줘가꾸 또 예약했는데 더 마음에 들어용☺️ 주변 친구들이 다니던 미용실 중 옆 뒤가 젤 깔끔하다고 하고, 스몰톡도 야무져서 지루하지도 않았어요. 상담 고민중이시면 꼭 추천드립니다.',
+        bgImg: BgImage,
+    },
+    {
+        title: '단발에서 긴머리로 변신 💇🏻‍♀️',
+        designer: '김서연 디자이너',
+        rating: 4.7,
+        review: '이번이 두번재 예약인데 기억해주시더라구요!! 일단 감동하면서 앉았습니다. 처음 자를 때 너무 깔꼼하게 잘라줘가꾸 또 예약했는데 더 마음에 들어용☺️ 주변 친구들이 다니던 미용실 중 옆 뒤가 젤 깔끔하다고 하고, 스몰톡도 야무져서 지루하지도 않았어요. 상담 고민중이시면 꼭 추천드립니다.',
+        bgImg: BgImage,
+    },
+]
 
 export default function BeforeAfterSection() {
     return (
@@ -8,9 +33,9 @@ export default function BeforeAfterSection() {
             </span>
             <div className='flex w-screen gap-12 -ml-20 overflow-x-scroll'>
                 <div className='w-8'></div>
-                <BeforeAfterCard />
-                <BeforeAfterCard />
-                <BeforeAfterCard />
+                {beforeAfterData.map((card) => (
+                    <BeforeAfterCard {...card} />
+                ))}
                 <div className='w-8'></div>
             </div>
         </section>
