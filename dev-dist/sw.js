24ac4610f860a91c9404a0723a96f95ef0e83fad
silverain <ekdldkaa@naver.com>
2025-02-17 13:32:34 +0900

    hotfix : public img 링크

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,9 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-
-    "revision": "0.182s1qk9km8"
-
+    "revision": "0.8b4o4s627k8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
