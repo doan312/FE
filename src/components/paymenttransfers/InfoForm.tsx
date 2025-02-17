a533610c5438cfb2f1dacc292785ecdf6abf9bf4
silverain <ekdldkaa@naver.com>
2025-02-17 15:50:38 +0900

    feat : getProfile api연동

--- src/components/paymenttransfers/InfoForm.tsx
+++ src/components/paymenttransfers/InfoForm.tsx
@@ -1,39 +1,49 @@
-import React from "react";
+import React from 'react'
+import { UserData } from '../../pages/designers/PaymentTransfer'
 
 interface InfoFormProps {
-  request: string;
-  setRequest: (value: string) => void;
+    request: string
+    setRequest: (value: string) => void
+    userData: UserData | null
 }
 
-const InfoForm: React.FC<InfoFormProps> = ({ request, setRequest }) => {
-  return (
-    <div className="mt-6 pt-2">
-      <p className="font-bold text-black font-xl">정보 입력</p>
-      <div className="mt-3 flex items-center gap-x-22 font-semibold">
-        <p className="text-gray-600 text-">이름</p>
-        <p className="font-sm text-black">박수현</p>
-      </div>
-      <div className="mt-1 flex items-center gap-x-22 font-semibold">
-        <p className="text-gray-600 text-md">이메일</p>
-        <p className="font-sm text-black">parksu@gmail.com</p>
-      </div>
+const InfoForm: React.FC<InfoFormProps> = ({
+    request,
+    setRequest,
+    userData,
+}) => {
+    return (
+        <div className='mt-6 pt-2'>
+            <p className='font-xl font-bold text-black'>정보 입력</p>
+            <div className='mt-3 flex items-center gap-x-22 font-semibold'>
+                <p className='text- text-gray-600'>이름</p>
+                <p className='font-sm text-black'>{userData?.name}</p>
+            </div>
+            <div className='mt-1 flex items-center gap-x-22 font-semibold'>
+                <p className='text-md text-gray-600'>이메일</p>
+                <p className='font-sm text-black'>{userData?.email}</p>
+            </div>
 
-      {/* 요청사항 입력 */}
-      <div className="mt-8">
-  <div className="flex justify-between items-center">
-    <p className="text-gray-900 text-md font-medium">요청사항</p>
-    <p className="text-gray-600 text-md">{request.length}/200</p>
-  </div>
-  <textarea
-  className="w-full h-36 p-3 bg-transparent border border-gray-500 rounded-xl mt-2 text-md placeholder-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-400"
-  placeholder="디자이너 분께 요청하실 내용을 적어주세요."
-  maxLength={200}
-  value={request}
-  onChange={(e) => setRequest(e.target.value)}
-/>
-</div>
-    </div>
-  );
-};
+            {/* 요청사항 입력 */}
+            <div className='mt-8'>
+                <div className='flex items-center justify-between'>
+                    <p className='text-md font-medium text-gray-900'>
+                        요청사항
+                    </p>
+                    <p className='text-md text-gray-600'>
+                        {request.length}/200
+                    </p>
+                </div>
+                <textarea
+                    className='text-md mt-2 h-36 w-full rounded-xl border border-gray-500 bg-transparent p-3 placeholder-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-400'
+                    placeholder='디자이너 분께 요청하실 내용을 적어주세요.'
+                    maxLength={200}
+                    value={request}
+                    onChange={(e) => setRequest(e.target.value)}
+                />
+            </div>
+        </div>
+    )
+}
 
-export default InfoForm;
+export default InfoForm
--- src/pages/designers/PaymentTransfer.tsx
+++ src/pages/designers/PaymentTransfer.tsx
@@ -1,116 +1,136 @@
-import React, { useState, useEffect } from "react";
-import PaymentModal from "../../components/paymenttransfers/PaymentModal";
-import InfoForm from "../../components/paymenttransfers/InfoForm"; // 분리된 폼 컴포넌트 import
-import DefaultHearImage from "../../assets/images/default_hear.png"; // 상대 경로
+import React, { useState, useEffect } from 'react'
+import PaymentModal from '../../components/paymenttransfers/PaymentModal'
+import InfoForm from '../../components/paymenttransfers/InfoForm' // 분리된 폼 컴포넌트 import
+import DefaultHearImage from '../../assets/images/default_hear.png' // 상대 경로
+import { useGetProfile } from '../../apis/api/get/useGetProfile'
+
+export interface UserData {
+    name: string
+    email: string
+}
 
 const PaymentTransfer: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [request, setRequest] = useState("");
+    const [showModal, setShowModal] = useState(false)
+    const [request, setRequest] = useState('')
+    const [userData, setUserData] = useState<UserData | null>(null)
 
-  useEffect(() => {
-    if (showModal) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [showModal]);
+    const res = useGetProfile()
 
-  return (
-    <div className="flex flex-col items-center min-h-screen bg-white">
-      {/* 상단 헤더 */}
-      <div className="fixed top-0 w-full bg-white py-4 px-2 flex items-center justify-between">
-        <button className="p-2 bg-transparent border-none">
-          <div className="w-6 h-6 flex items-center justify-center bg-transparent">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 5L8 12L15 19"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </div>
-        </button>
-        <h2 className="text-lg font-semibold text-black">예약하기</h2>
-        <div className="w-6"></div> {/* 우측 여백 맞추기 */}
-      </div>
-      {/* 본문 영역 */}
-      <div className="w-full max-w-md mt-16 px-4 pt-4">
-        {/* 디자이너 정보 */}
+    useEffect(() => {
+        if (res.isSuccess && res.data !== null) {
+            setUserData(res.data.data.data)
+        }
+    }, [res.isSuccess])
 
+    useEffect(() => {
+        if (showModal) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'auto'
+        }
+        return () => {
+            document.body.style.overflow = 'auto'
+        }
+    }, [showModal])
 
-        <div className="w-full h-44 rounded-lg mb-6 overflow-hidden">
-      <img
-        src={DefaultHearImage}
-        alt="디자이너 이미지"
-        className="w-full h-full object-cover"
-      />
-    </div>
-    </div>
-          {/* 본문 영역 */}
-          <div className="w-full max-w-md px-5">
-        {/* 디자이너 정보 */}
-        <h3 className="text-2xl font-bold text-black">박수연 실장</h3>
-        <p className="text-black text-md">준오헤어 반포점</p>
+    return (
+        <div className='flex min-h-screen flex-col items-center bg-white'>
+            {/* 상단 헤더 */}
+            <div className='fixed top-0 flex w-full items-center justify-between bg-white px-2 py-4'>
+                <button className='border-none bg-transparent p-2'>
+                    <div className='flex h-6 w-6 items-center justify-center bg-transparent'>
+                        <svg
+                            width='24'
+                            height='24'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                            xmlns='http://www.w3.org/2000/svg'>
+                            <path
+                                d='M15 5L8 12L15 19'
+                                stroke='black'
+                                strokeWidth='2'
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                            />
+                        </svg>
+                    </div>
+                </button>
+                <h2 className='text-lg font-semibold text-black'>예약하기</h2>
+                <div className='w-6'></div> {/* 우측 여백 맞추기 */}
+            </div>
+            {/* 본문 영역 */}
+            <div className='mt-16 w-full max-w-md px-4 pt-4'>
+                {/* 디자이너 정보 */}
 
-        {/* 일정 정보 */}
-        <div className="flex items-center gap-x-16 mt-2 pb-6 ">
-          <p className="text-gray-600 text-md font-semibold">일정</p>
-          <p className="font-semibold text-lg text-black">05.26(일) 오후 1:00~오후 1:30</p>
+                <div className='mb-6 h-44 w-full overflow-hidden rounded-lg'>
+                    <img
+                        src={DefaultHearImage}
+                        alt='디자이너 이미지'
+                        className='h-full w-full object-cover'
+                    />
+                </div>
+            </div>
+            {/* 본문 영역 */}
+            <div className='w-full max-w-md px-5'>
+                {/* 디자이너 정보 */}
+                <h3 className='text-2xl font-bold text-black'>박수연 실장</h3>
+                <p className='text-md text-black'>준오헤어 반포점</p>
+
+                {/* 일정 정보 */}
+                <div className='mt-2 flex items-center gap-x-16 pb-6'>
+                    <p className='text-md font-semibold text-gray-600'>일정</p>
+                    <p className='text-lg font-semibold text-black'>
+                        05.26(일) 오후 1:00~오후 1:30
+                    </p>
+                </div>
+                {/* 구분선 추가 */}
+                <div className='relative mb-7 w-full'>
+                    <div className='absolute left-0 w-full border-t-1 border-gray-400'></div>
+                </div>
+                {/* 결제 금액 */}
+                <div className='flex w-full items-center justify-between pb-12'>
+                    <p className='text-xl font-bold text-gray-600'>결제금액</p>
+                    <div className='flex items-center gap-x-1'>
+                        <span className='mr-1.5 rounded-md bg-blue-100 px-2 py-1 text-xs font-semibold text-blue-600'>
+                            온라인
+                        </span>
+                        <p className='text-xl font-bold text-purple-600'>
+                            45,000원
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* 구분선 추가 */}
+            <div className='w-full border-t-10 border-gray-400'></div>
+            {/* 본문 영역 */}
+            <div className='w-full max-w-md px-5 pb-40'>
+                {/* 정보 입력 폼 (컴포넌트 사용) */}
+                <InfoForm
+                    request={request}
+                    setRequest={setRequest}
+                    userData={userData}
+                />
+            </div>
+            {/* 결제 버튼 */}
+            <div className='fixed bottom-0 left-0 w-full bg-white'>
+                {/* 구분선 */}
+                <div className='absolute left-0 top-0 w-full border-t-1 border-gray-400'></div>
+
+                {/* 버튼 영역 */}
+                <div className='p-4'>
+                    <button
+                        className='w-full rounded-xl bg-black py-4 text-lg font-semibold text-white'
+                        onClick={() => setShowModal(true)}>
+                        결제하고 예약 확정
+                    </button>
+                </div>
+            </div>
+
+            {/* 모달 표시 */}
+            {showModal && <PaymentModal onClose={() => setShowModal(false)} />}
         </div>
-{/* 구분선 추가 */}
-<div className="relative w-full mb-7">
-  <div className="absolute left-0 w-full border-t-1 border-gray-400"></div>
-</div>
-        {/* 결제 금액 */}
-        <div className="flex items-center justify-between w-full pb-12">
-          <p className="text-gray-600 text-xl font-bold">결제금액</p>
-          <div className="flex items-center gap-x-1">
-            <span className="px-2 py-1 text-xs font-semibold text-blue-600 bg-blue-100 rounded-md mr-1.5">
-              온라인
-            </span>
-            <p className="text-purple-600 font-bold text-xl">45,000원</p>
-          </div>
-        </div>
-        </div>
+    )
+}
 
-{/* 구분선 추가 */}
-<div className="w-full border-t-10 border-gray-400"></div>
-   {/* 본문 영역 */}
-   <div className="w-full max-w-md px-5 pb-40">
-        {/* 정보 입력 폼 (컴포넌트 사용) */}
-        <InfoForm request={request} setRequest={setRequest} />
-        </div> 
-    {/* 결제 버튼 */}
-<div className="fixed bottom-0 left-0 w-full bg-white">
-  {/* 구분선 */}
-  <div className="w-full border-t-1 border-gray-400 absolute top-0 left-0"></div>
-
-  {/* 버튼 영역 */}
-  <div className="p-4 ">
-    <button
-      className="w-full py-4 bg-black text-white text-lg font-semibold rounded-xl"
-      onClick={() => setShowModal(true)}
-    >
-      결제하고 예약 확정
-    </button>
-  </div>
-</div>
-
-      {/* 모달 표시 */}
-      {showModal && <PaymentModal onClose={() => setShowModal(false)} />}
-    </div>
-  );
-};
-
-export default PaymentTransfer;
+export default PaymentTransfer
