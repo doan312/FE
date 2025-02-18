f3684eab9ee61a2b371bb0d47942b3e84ac62552
Jisun Park <jisunpark.t@gmail.com>
2025-02-18 20:16:37 +0900

    design: 스크롤 시 헤더영역 백그라운드 블러

--- src/components/home/MainSection.tsx
+++ src/components/home/MainSection.tsx
@@ -12,7 +12,7 @@ export default function MainSection() {
     const visibleDesignerCards = designersData.slice(0, displayCount)
 
     return (
-        <div className='flex flex-col mb-44'>
+        <div className='flex flex-col mb-44 mt-116'>
             <h2 className='mb-16 font-bold text-h2 text-gray-1300'>
                 어떤 방식으로 상담 받을까요?
             </h2>
