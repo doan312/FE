4385381e389101ffcebd8bc0e8cc5fbce4beae79
Jisun Park <jisunpark.t@gmail.com>
2025-02-19 04:25:49 +0900

    feat: 스와이퍼 버튼 기능 구현

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -5,6 +5,8 @@ import { Pagination } from 'swiper/modules'
 import OnBoarding01 from '../../public/img/onboarding_1.png'
 import OnBoarding02 from '../../public/img/onboarding_2.png'
 import OnBoarding03 from '../../public/img/onboarding_3.png'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const onBoardingData = [
     {
@@ -46,6 +48,15 @@ const onBoardingData = [
 ]
 
 const Onboarding: React.FC = () => {
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [my_swiper, set_my_swiper] = useState<any>({})
+    const navigate = useNavigate()
+
+    const handleButtonClick = () => {
+        if (currentIndex === 2) navigate('/')
+        my_swiper.slideNext()
+    }
+
     return (
         <div className='relative w-full h-screen bg-gray-100 onboarding'>
             <Swiper
@@ -53,7 +64,11 @@ const Onboarding: React.FC = () => {
                 grabCursor={true}
                 className='h-[calc(100vh-88px)]'
                 modules={[Pagination]}
-                pagination={{ clickable: true }}>
+                pagination={{ clickable: true }}
+                onInit={(ev) => {
+                    set_my_swiper(ev)
+                }}
+                onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}>
                 {onBoardingData.map((slide) => (
                     <SwiperSlide>
                         <div className='h-full overflow-hidden pt-57'>
@@ -74,8 +89,9 @@ const Onboarding: React.FC = () => {
             <div className='absolute bottom-0 left-0 right-0 px-16 pt-10 h-88 border-t-1 border-t-gray-300 pb-30'>
                 <button
                     type='button'
+                    onClick={handleButtonClick}
                     className={`flex h-48 w-full items-center justify-center rounded-10 bg-gray-1200 text-body1 font-medium text-gray-100`}>
-                    다음
+                    {currentIndex === 2 ? '시작하기' : '다음'}
                 </button>
             </div>
         </div>
