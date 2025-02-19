1616efbf4c2cd33d282790fc9f0455d4b9a1df07
silverain <ekdldkaa@naver.com>
2025-02-19 15:31:33 +0900

    hotfix : 디자이너 상세 버튼 위치

--- src/components/designerDetail/ButtonLg.tsx
+++ src/components/designerDetail/ButtonLg.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 interface ButtonLgProps {
     text: string
+    available: boolean
 }
 
-const ButtonLg: React.FC<ButtonLgProps> = ({ text }) => {
+const ButtonLg: React.FC<ButtonLgProps> = ({ text, available }) => {
     return (
-        <div className='mb-[1rem] ml-[1rem] mr-[1rem] mt-[0.62rem] flex h-[3rem] items-center justify-center rounded-xl bg-gray-1200 text-body2 font-medium text-white'>
+        <div
+            className={`fixed bottom-[1.88rem] z-30 mb-[1rem] ml-[1rem] mr-[1rem] mt-[0.62rem] flex h-[3rem] w-[90%] items-center justify-center rounded-xl text-body2 font-medium ${
+                available ? 'bg-gray-1200 text-white' : 'bg-gray-600 text-white'
+            }`}>
             {text}
         </div>
     )
