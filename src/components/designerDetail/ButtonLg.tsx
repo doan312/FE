e03d478d0a9d2549c745aa7ad56eb7a62495f83a
silverain <ekdldkaa@naver.com>
2025-02-19 16:47:07 +0900

    feat : 예약 후 페이지 이동

--- src/components/designerDetail/ButtonLg.tsx
+++ src/components/designerDetail/ButtonLg.tsx
@@ -6,8 +6,12 @@ interface ButtonLgProps {
 }
 
 const ButtonLg: React.FC<ButtonLgProps> = ({ text, available }) => {
+    const handleClick = () => {
+        window.location.href = '/paymenttransfer'
+    }
     return (
         <div
+            onClick={handleClick}
             className={`fixed bottom-[1.88rem] left-20 right-20 z-30 mx-auto mb-[1rem] mt-[0.62rem] flex h-[3rem] w-[calc(100%-40px)] max-w-[480px] items-center justify-center rounded-xl text-body2 font-medium ${
                 available ? 'bg-gray-1200 text-white' : 'bg-gray-600 text-white'
             }`}>
