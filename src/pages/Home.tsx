956cc2ac0d0c92e01368870ae5ebd8aa6b8d5ceb
silverain <ekdldkaa@naver.com>
2025-02-19 16:10:18 +0900

    feat : 로그인 여부에 따른 화면 이동 처리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 // 홈 화면
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Logo from '../components/home/Logo'
 import MainSection from '../components/home/MainSection'
 import RescheduleBanner from '../components/home/RescheduleBanner'
@@ -8,10 +8,19 @@ import HotNewSection from '../components/home/HotNewSection'
 import BeforeAfterSection from '../components/home/BeforeAfterSection'
 import TabBar from '../components/TabBar/TabBar'
 import BannerSwiper from '../components/home/BannerSwiper'
+import { useAccessTokenStore } from '../store/useStore'
 
 const Home: React.FC = () => {
+    //사용자 로그인 여부에 따른 화면 분기처리
+    const { accessToken } = useAccessTokenStore()
+    useEffect(() => {
+        if (!accessToken) {
+            window.location.href = '/login'
+        }
+    }, [accessToken])
+
     return (
-        <div className='relative z-0 w-full px-20 overflow-hidden bg-gray-100 pb-155'>
+        <div className='relative z-0 w-full overflow-hidden bg-gray-100 px-20 pb-155'>
             <div className='absolute left-0 right-0 top-0 -z-20 -m-16 h-[852px] bg-gradient-to-b from-[#E8DCFD] to-transparent'></div>
             <Logo />
             <MainSection />
