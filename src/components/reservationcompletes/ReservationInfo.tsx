d426adbe1e68fdc856e72858636a9ffc6248cd5e
silverain <ekdldkaa@naver.com>
2025-02-20 12:33:39 +0900

    hotfix : 예약 완료 하드코딩

--- src/components/reservationcompletes/ReservationInfo.tsx
+++ src/components/reservationcompletes/ReservationInfo.tsx
@@ -1,43 +1,15 @@
 import React from 'react'
-import { useReservationCompleteStore } from '../../store/useReservationCompleteStore'
-import dayjs from 'dayjs'
-import 'dayjs/locale/ko'
 
 const ReservationInfo: React.FC = () => {
-    const { bookingDate, bookingTime, meetingType } =
-        useReservationCompleteStore()
-    const isOnline = meetingType === 'REMOTE'
-
-    console.log(bookingDate, bookingTime, meetingType)
-
-    // 날짜와 시간을 포맷팅
-    const formattedDate = dayjs(bookingDate).locale('ko').format('MM.DD(ddd)')
-    const formattedTime = dayjs(`${bookingDate}T${bookingTime}`)
-        .locale('ko')
-        .format('A h:mm')
-
-    console.log(formattedDate, formattedTime)
-
     return (
         <div className='mx-auto mt-[16px] inline-flex w-fit rounded-[16px] border-[1px] border-gray-500 bg-transparent p-[16px]'>
             {/* 한 줄 정렬 */}
             <div className='flex items-center gap-x-[8px] whitespace-nowrap'>
-                {isOnline ? (
-                    <span className='mr-[4px] rounded-[6px] bg-blue-100 px-[8px] py-[4px] text-[14px] font-semibold text-blue-600'>
-                        온라인
-                    </span>
-                ) : (
-                    <span
-                        className='mr-[4px] rounded-[6px] px-[8px] py-[4px] text-[14px] font-semibold'
-                        style={{
-                            backgroundColor: '#FFECEC',
-                            color: '#FE6E81',
-                        }}>
-                        직접
-                    </span>
-                )}
+                <span className='mr-[4px] rounded-[6px] bg-blue-100 px-[8px] py-[4px] text-[14px] font-semibold text-blue-600'>
+                    온라인
+                </span>
                 <p className='text-[18px] font-semibold text-gray-950'>
-                    {formattedDate} {formattedTime}
+                    05.26(일) 오후 1:00~오후 1:30
                 </p>
             </div>
         </div>
