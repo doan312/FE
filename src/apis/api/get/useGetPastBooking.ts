cca487e6ef69579c4fad3e66008633407d2f0d4e
silverain <ekdldkaa@naver.com>
2025-02-20 14:06:25 +0900

    feat : 예약조회 구현

--- src/apis/api/get/useGetPastBooking.ts
+++ src/apis/api/get/useGetPastBooking.ts
@@ -14,7 +14,7 @@ export const useGetPastBooking = () => {
         queryKey: ['pastBooking'], // React Query 캐싱 키
         queryFn: async () => {
             const res = await authApi.get('/booking/past') // ✅ GET 요청
-            return res.data
+            return res.data.data
         },
         staleTime: 1000 * 60 * 5, // 5분 동안 캐싱된 데이터 유지
     })
